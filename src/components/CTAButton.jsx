3fad5b5021b0dc729cbcc0f94b9e61d42e2d002e
Phurpa Sherpa <phurpasherpa@Phurpas-MacBook-Air.local>
2026-07-19 22:05:10 +0545

    fix

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -11,7 +11,7 @@ export default function CTAButton({label='Order Now', link='/products',
         <span className={`${span} relative  2xl:text-lg font-medium z-10 2xl:tracking-wide`}>{label}</span>
         <div className='relative flex items-center justify-center'>
             <div className={`rounded-full h-8 w-8 2xl:h-10 2xl:w-10
-            group-hover:w-400 2xl:group-hover:w-[600px] 
+            group-hover:w-400 2xl:group-hover:w-[800px] 
             group-hover:h-60 2xl:group-hover:h-30
                 transition-all duration-400 ease-in
                 absolute ${className2}`}/>
